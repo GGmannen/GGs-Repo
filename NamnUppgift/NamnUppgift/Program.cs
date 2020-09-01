d56a61b18be82ea7a83743b84f3ef7cda88013b9
GGmannen <gustav.loof@elev.ga.ntig.se>
2020-09-01 12:30:45 +0200

    Namnuppgiften är uppdaterad och innehåller loopar och switch

--- NamnUppgift/NamnUppgift/Program.cs
+++ NamnUppgift/NamnUppgift/Program.cs
@@ -42,13 +42,49 @@ namespace NamnUppgift
 
             }
 
-            else
+            else if(age < 10)
             {
 
-                Console.WriteLine("Du är inte så gammal, grattis! Du är: " + age + "år gammal");
+                Console.WriteLine("Du är var mysteriskt ung! Du är: " + age + "år gammal");
 
             }
-            
+            else
+            {
+                Console.WriteLine("Du är ganska ung, grattis! Du är:" + age + "år gammal");
+            }
+
+            for(var i = 0; i > 10; i++)
+            {
+                Console.WriteLine(i);
+
+            }
+
+            int choose = 0;
+
+            while (choose > 1 || choose < 3)
+            {
+                Console.WriteLine("Ange en siffra mellan 1 och 3");
+
+                choose = Convert.ToInt32(Console.ReadLine());
+
+                switch (choose)
+                {
+                    case 1: Console.WriteLine("Du valde fall 1, där händer inget alls!");
+                        break;
+
+                    case 2: Console.WriteLine("Du valde fall 2, där händer det inte nånting alls heller!");
+                        break;
+
+                    case 3: Console.WriteLine("Du valde fall 3, och till din stora överaskning så hände inget där heller!");
+                            break;
+
+                }
+                break;
+
+
+            }
+
+            Console.WriteLine("Slut!");
         }
     }
 }
