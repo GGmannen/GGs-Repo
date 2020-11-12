95b7774c29db9eb55b904ee2c2d85d1ad0a90cb1
GGmannen <gustav.loof@elev.ga.ntig.se>
2020-11-12 10:53:54 +0100

    Miniräkaren

---
+++ Calculator lab/Calculator lab/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator_lab
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        void OnClick (object sender, RoutedEventArgs e)
+        {
+            if (sender is Button knapp)
+            {
+                switch (true)
+                {
+                    case "0":
+                    case "1":
+                    case "2":
+                    case "3":
+                    case "4":
+                    case "5":
+                    case "6":
+                    case "7":
+                    case "8":
+                    case "9":
+                       
+                        break;
+
+
+                    default:
+                        break;
+                }
+
+
+            }
+        }
+    }
+}
