cbb4c534b276e0e2acec64a10c106c8b3a10fbc4
GGmannen <gustav.loof@elev.ga.ntig.se>
2021-04-22 10:23:35 +0200

    Första commit för flappy bird projekt
    I denna commit så har jag lagt in själva bilderna för spelet som tex rören och själva fågeln men även moln. Jag har även påbörjat koden för funktioner där jag har lagt in en timer och definerat lite olika variabler  som tex "gravity"

---
+++ Flappy bird WPF/Flappy bird WPF/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+using System.Windows.Threading;
+
+namespace Flappy_bird_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        DispatcherTimer gameTimer = new DispatcherTimer();
+
+        double score;
+        int gravity = 8;
+        bool gameOver;
+        Rect flappyBirdHitBox;
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            gameTimer.Tick += MainEventTimer;
+
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void StartGame()
+        {
+
+        }
+
+        private void EndGame()
+        {
+
+        }
+
+
+    }
+}
